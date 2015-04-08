099aa1230e6c1d5ad79056dd74c9d6e8446c325e
butterandfly <zero.hero.lin@gmail.com>
2015-04-08 14:00:57 +0000

    init

---
+++ el-get/.status.el
@@ -0,0 +1,51 @@
+((auto-complete status "installed" recipe
+		(:name auto-complete :website "https://github.com/auto-complete/auto-complete" :description "The most intelligent auto-completion extension." :type github :pkgname "auto-complete/auto-complete" :depends
+		       (popup fuzzy)
+		       :features auto-complete-config :post-init
+		       (progn
+			 (add-to-list 'ac-dictionary-directories
+				      (expand-file-name "dict" default-directory))
+			 (ac-config-default))))
+ (el-get status "installed" recipe
+	 (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "master" :pkgname "dimitri/el-get" :info "." :compile
+		("el-get.*\\.el$" "methods/")
+		:features el-get :post-init
+		(when
+		    (memq 'el-get
+			  (bound-and-true-p package-activated-list))
+		  (message "Deleting melpa bootstrap el-get")
+		  (unless package--initialized
+		    (package-initialize t))
+		  (when
+		      (package-installed-p 'el-get)
+		    (let
+			((feats
+			  (delete-dups
+			   (el-get-package-features
+			    (el-get-elpa-package-directory 'el-get)))))
+		      (el-get-elpa-delete-package 'el-get)
+		      (dolist
+			  (feat feats)
+			(unload-feature feat t))))
+		  (require 'el-get))))
+ (evil status "installed" recipe
+       (:name evil :website "http://gitorious.org/evil/pages/Home" :description "Evil is an extensible vi layer for Emacs. It\n       emulates the main features of Vim, and provides facilities\n       for writing custom extensions." :type git :url "http://gitorious.org/evil/evil.git" :features evil :depends
+	      (undo-tree goto-chg)
+	      :build
+	      (("make" "info" "all"))
+	      :build/berkeley-unix
+	      (("gmake" "info" "all"))
+	      :build/darwin
+	      `(("make" ,(format "EMACS=%s" el-get-emacs)
+		 "info" "all"))
+	      :info "doc"))
+ (fuzzy status "installed" recipe
+	(:name fuzzy :website "https://github.com/auto-complete/fuzzy-el" :description "Fuzzy matching utilities for GNU Emacs" :type github :pkgname "auto-complete/fuzzy-el"))
+ (go-mode status "installed" recipe
+	  (:name go-mode :description "Major mode for the Go programming language" :type github :pkgname "dominikh/go-mode.el"))
+ (goto-chg status "installed" recipe
+	   (:name goto-chg :description "Goto the point of the most recent edit in the buffer." :type emacswiki :features goto-chg))
+ (popup status "installed" recipe
+	(:name popup :website "https://github.com/auto-complete/popup-el" :description "Visual Popup Interface Library for Emacs" :type github :submodule nil :pkgname "auto-complete/popup-el"))
+ (undo-tree status "installed" recipe
+	    (:name undo-tree :description "Treat undo history as a tree" :website "http://www.dr-qubit.org/emacs.php" :type git :url "http://www.dr-qubit.org/git/undo-tree.git/")))
